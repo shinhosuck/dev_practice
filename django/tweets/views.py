4122073f2a03883030a40d13953d76843de3e5b6
shinhosuck <shinhosuck1973@gamil.com>
2023-01-06 15:17:27 -0800

    update

--- django/tweets/views.py
+++ django/tweets/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from tweets.forms import TweetForm
-
+from tweets.models import Tweet
 def home_view(request):
     return render(request, 'tweets/home.html', context=None)
 
 
 def tweet_list_view(request):
-    return JsonResponse({'name':'Jane Does'})
+    all_tweets = Tweet.objects.all().order_by('-id')
+    tweets = []
+    for tweet in all_tweets:
+        tweets.append({'id': tweet.id, 'content': tweet.content})
+    data = {
+        'tweets': tweets
+    }
+    return JsonResponse(data)
 
 
 def tweet_create_view(request):
-    print(request.POST.get('content'))
+    print(request.is_ajax())
     if request.method == 'POST':
         form = TweetForm(request.POST)
         if form.is_valid():
