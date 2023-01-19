4a1839ab8f7037eec4bb5261fca81fab0c9ff1f6
shinhosuck <shinhosuck1973@gmail.com>
2023-01-19 17:10:58 +0800

    update

--- django/tweets/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from tweets.models import Tweet
-
-
-
-admin.site.register(Tweet)
--- django/tweets/forms.py
+++
@@ -1,9 +0,0 @@
-from tweets.models import Tweet
-from django import forms
-
-
-
-class tweetCreateForm(forms.ModelForm):
-    class Meta:
-        model = Tweet
-        fields = ['title', 'content']
