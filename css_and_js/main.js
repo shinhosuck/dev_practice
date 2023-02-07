b326d938f74b3509f06e670ef338eb0a3935c206
shinhosuck <shinhosuck1973@hotmail.com>
2023-02-08 05:03:15 +0800

    update

--- css_and_js/main.js
+++ css_and_js/main.js
@@ -1,19 +1,72 @@
-const container = document.querySelector('.container')
-const cbBtn = document.querySelector('#cb-btn')
 
-const colors = ['orange', 'purple', 'blue', 'red']
-let index = 0
+const navItems = document.querySelector('.nav-items')
+const barsBtn = document.querySelector('.bars-btn')
+const timesBtn = document.querySelector('.times-btn')
+const body = document.querySelector('body')
 
-container.style.background = colors[index]
 
-cbBtn.addEventListener('click', changeBackground)
 
-function changeBackground(event) {
-    if(index == colors.length-1) {
-        index = 0
+barsBtn.addEventListener('click', function() {
+    barsBtn.style.display = 'none'
+    timesBtn.style.display = 'inline-block'
+
+    navItems.classList.add('show-nav-items')
+    body.style.overflowY= 'hidden'
+    // mobileSearchForm.style.display = 'block'
+
+})
+
+timesBtn.addEventListener('click', function() {
+    barsBtn.style.display = 'inline-block'
+    timesBtn.style.display = 'none'
+
+    navItems.classList.remove('show-nav-items')
+    body.style.overflowY = 'auto'
+    // mobileSearchForm.style.display = 'none'
+})
+
+window.addEventListener('resize', function(){
+    if(window.innerWidth >= 1000){
+        barsBtn.style.display = 'inline-block'
+        timesBtn.style.display = 'none'
+        navItems.classList.remove('show-nav-items')
+        body.style.overflowY = 'auto'
     }
-    else if(index < colors.length) {
-        index ++
-    }
-    container.style.background = colors[index]
-}
+})
+
+
+const navContainer = document.querySelector('.nav-container')
+console.log(navContainer.offsetHeight)
+
+const searchForm = document.querySelector('#search-form')
+const data = document.querySelector('#data')
+
+searchForm.addEventListener('submit', function(event){
+    event.preventDefault()
+    const searched = data.value
+    searchForm.reset()
+
+    // window.location.href = `https://www.google.com/search?q=${data}`
+    window.open(
+        `https://www.google.com/search?q=${searched}`, '_blank'
+    )
+})
+
+const mobileSearch = document.querySelector('#mobile-search')
+const mobileData = document.querySelector('#mobile-data')
+
+mobileSearch.addEventListener('submit', function(event){
+    event.preventDefault()
+    const searched = mobileData.value
+    searchForm.reset()
+
+    // window.location.href = `https://www.google.com/search?q=${data}`
+    window.open(
+        `https://www.google.com/search?q=${searched}`, '_blank'
+    )
+})
+
+const num = ['s', 'z', 'c', 'r', 't', 'i']
+
+num.sort()
+console.log(num)
