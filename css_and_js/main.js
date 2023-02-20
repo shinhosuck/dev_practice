63f3523f7c9ed87c856af350aaebffee7353efef
shinhosuck <shinhosuck1973@gmail.com>
2023-02-21 02:44:26 +0800

    update

--- css_and_js/main.js
+++ css_and_js/main.js
@@ -4,6 +4,7 @@ const barsBtn = document.querySelector('.bars-btn')
 const timesBtn = document.querySelector('.times-btn')
 const body = document.querySelector('body')
 
+const main = document.querySelector('main')
 
 
 barsBtn.addEventListener('click', function() {
@@ -70,3 +71,19 @@ mobileSearch.addEventListener('submit', function(event){
 const h1 = [...document.querySelectorAll('h1')]
 const h2 = [...document.querySelectorAll('h2')]
 const h3 = [...document.querySelectorAll('h3')]
+
+
+window.addEventListener('scroll', function(){
+    // location of the scroll bar
+    console.log(window.pageYOffset)
+})
+
+// full html document height
+console.log('Document Height:', document.body.scrollHeight)
+// windows view port height (vh)
+console.log('Window View Port Height:', window.innerHeight)
+// element offset height
+console.log('Main First Child Offset Height', main.children[0].offsetHeight)
+// element client height
+console.log('Main First Child Client Height', main.children[0].clientHeight)
+
